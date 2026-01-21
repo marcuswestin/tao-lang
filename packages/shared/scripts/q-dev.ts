1b0f492df30433954a97d245026aec97d33f5111
Marcus Westin <marcus.westin@gmail.com>
2026-01-21 18:51:25 -0500

    feat(ide): add markdown syntax highlighting for tao code blocks
    - Add tao-lang.markdown-embed.tmLanguage.json for fenced code block injection
    - Register markdown injection grammar in IDE extension package.json
    - Rename merge-tm-files to gen-syntax-tmLanguage-files and extend to copy markdown embed
    - Run compiler gen before packaging IDE extension

--- packages/shared/scripts/q-dev.ts
+++ packages/shared/scripts/q-dev.ts
@@ -1,10 +1,10 @@
 import { Command } from '@commander-js/extra-typings'
 import { genMiseTasks } from './commands/gen-mise-tasks'
-import { mergeTmFiles } from './commands/merge-tm-files'
+import { genSyntaxTmLanguageFiles } from './commands/gen-syntax-tmLanguage-files'
 
 const program = new Command()
 
-function ideSynPath(path: string) {
+function genDefaultIdeSyntaxPathValue(path: string) {
   return __dirname + '/../../ide-extension/ide-syntaxes/' + path
 }
 
@@ -14,25 +14,30 @@ program.command('gen-mise-tasks')
     await genMiseTasks()
   })
 
-program.command('merge-tm-files')
+program.command('gen-syntax-tmLanguage-files')
   .description('Merge tm files to include TS code blocks')
-  .argument(
-    '[tmJsonPath]',
-    'The path to the tm json file',
-    ideSynPath('_gen-syntaxes/tao-lang.tmLanguage.json'),
-  )
   .argument(
     '[mergeJsonPath]',
     'The path to the merge json file',
-    ideSynPath('tao-lang.ts-code-blocks-merge.tmLanguage.json'),
+    genDefaultIdeSyntaxPathValue('tao-lang.ts-code-blocks-merge.tmLanguage.json'),
   )
   .argument(
-    '[outputJsonPath]',
-    'The path to the output json file',
-    ideSynPath('_gen-syntaxes/tao-lang.tmLanguage.json'),
+    '[markdownEmbedJsonPath]',
+    'The path to the markdown embed json file',
+    genDefaultIdeSyntaxPathValue('tao-lang.markdown-embed.tmLanguage.json'),
   )
-  .action(async (tmJsonPath, mergeJsonPath, outputJsonPath) => {
-    await mergeTmFiles(tmJsonPath, mergeJsonPath, outputJsonPath)
+  .argument(
+    '[tmJsonPath]',
+    'The path to the tm json file',
+    genDefaultIdeSyntaxPathValue('_gen-syntaxes/tao-lang.tmLanguage.json'),
+  )
+  .argument(
+    '[outputDirPath]',
+    'The path to the output directory',
+    genDefaultIdeSyntaxPathValue('_gen-syntaxes'),
+  )
+  .action(async (mergeJsonPath, markdownEmbedJsonPath, tmJsonPath, outputDirPath) => {
+    await genSyntaxTmLanguageFiles(tmJsonPath, mergeJsonPath, markdownEmbedJsonPath, outputDirPath)
   })
 
 program.parse(Bun.argv)
